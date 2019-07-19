902bacd95d4462a9f3948df3a91d38272d8331ba
Christopher Plumberg <christopher.plumberg@thep.lu.se>
2019-07-19 16:00:46 +0200

    Edits to submit.sh.

--- submit.sh
+++ submit.sh
@@ -3,4 +3,15 @@
 chosen_OMP_NUM_THREADS=2
 echo 'chosen_OMP_NUM_THREADS='$chosen_OMP_NUM_THREADS > omp_env.sh
 
-( ./compile_all.sh $chosen_OMP_NUM_THREADS &> compile_all.out && ./driver.sh useParallel=true projectile="Pb" target="Pb" beamEnergy="2760.0" Nevents=10 ThermalOnly='true' SetPartonVertices='off' &> driver.out ) &
+( ./compile_all.sh              \
+	$chosen_OMP_NUM_THREADS \
+	&> compile_all.out      \
+  && ./driver.sh                \
+	useParallel=true        \
+	projectile="Pb"         \
+	target="Pb"             \
+	beamEnergy="2760.0"     \
+	Nevents=10              \
+	ThermalOnly='true'      \
+	SetPartonVertices='off' \
+	&> driver.out ) &
