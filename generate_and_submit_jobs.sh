203d3b2a83f878d0c2564d0c7b291aec64c13d04
Christopher Plumberg <christopher.plumberg@thep.lu.se>
2019-09-19 14:52:34 +0200

    About to remove unneeded source files.

--- generate_and_submit_jobs.sh
+++ generate_and_submit_jobs.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
 (
-	./generate_jobs.sh 1 this_is_a_test
+	./generate_jobs.sh 1 this_is_still_a_test
 
-	./submit_jobs.sh this_is_a_test
+	./submit_jobs.sh this_is_still_a_test
 ) &> /dev/null &
 
 # End of file
