7bb12ab20f6412381ea9a442f60bf989b87d67c5
Christopher Plumberg <plumberg.1@osu.edu>
2020-03-17 16:17:55 +0100

    Add better random event generator for testing shifter

--- src/HBT_event_generator/HBT_event_generator_w_errors/src/random_events.h
+++ src/HBT_event_generator/HBT_event_generator_w_errors/src/random_events.h
@@ -108,11 +108,12 @@ void generate_events_v2(vector<EventRecord> & allEvents, ParameterReader * paraR
 
 	normal_distribution<double> distribution(0.0,RNG_R/sqrt(2.0));
 
-	int model_to_use = 0;
+	int model_to_use = 3;
 	// model_to_use values:
 	//		0 - Zhang, Wiedemann, Slotta, and Heinz (1997) or slight variant
 	//		1 - my own toy emission function
 	//		2 - another of my own toy emission functions
+	//		3 - purely Gaussian, uncorrelated spectra and positions
 
 	if ( model_to_use == 0 )
 	{
@@ -311,6 +312,64 @@ void generate_events_v2(vector<EventRecord> & allEvents, ParameterReader * paraR
 
 			allEvents.push_back( event );
 
+		}
+	}
+	else if ( model_to_use == 2 )
+	{
+		double mass 	= paraRdr->getVal("mass");
+		double RNG_R 	= paraRdr->getVal("RNG_R");
+		double RNG_a 	= paraRdr->getVal("RNG_a");
+
+		int RNG_Nev 	= paraRdr->getVal("RNG_Nev");
+		int RNG_mult 	= paraRdr->getVal("RNG_mult");
+		int RNG_xDir 	= paraRdr->getVal("RNG_xDir");
+		int RNG_yDir 	= paraRdr->getVal("RNG_yDir");
+		int RNG_zDir 	= paraRdr->getVal("RNG_zDir");
+
+		bool RNG_seed	= (bool)paraRdr->getVal("RNG_seed");
+
+		unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+		default_random_engine generator;
+		if ( RNG_seed )
+			generator = default_random_engine (seed);
+
+		normal_distribution<double> distribution(0.0, RNG_R);
+
+		for (int iEvent = 0; iEvent < RNG_Nev; ++iEvent)
+		{
+			EventRecord event;
+		
+			for (int iParticle = 0; iParticle < RNG_mult; ++iParticle)
+			{
+			
+				double tP = 0.0;	// cf. paper
+				double xP = RNG_xDir ? distribution(generator) : 0.0;
+				double yP = RNG_yDir ? distribution(generator) : 0.0;
+				double zP = RNG_zDir ? distribution(generator) : 0.0;
+
+				double px = RNG_xDir ? RNG_a * distribution(generator) : 0.0;
+				double py = RNG_yDir ? RNG_a * distribution(generator) : 0.0;
+				double pz = RNG_zDir ? RNG_a * distribution(generator) : 0.0;
+				double Ep = sqrt( mass*mass + px*px + py*py + pz*pz );
+
+				ParticleRecord particle;
+				particle.eventID 	= iEvent;
+				particle.particleID = iParticle;
+				particle.E 			= Ep;
+				particle.px 		= px;
+				particle.py 		= py;
+				particle.pz 		= pz;
+				particle.t 			= tP;
+				particle.x 			= xP;
+				particle.y 			= yP;
+				particle.z 			= zP;
+
+				event.particles.push_back( particle );
+
+			} 
+
+			allEvents.push_back( event );
+		
 		}
 	}
 	else
