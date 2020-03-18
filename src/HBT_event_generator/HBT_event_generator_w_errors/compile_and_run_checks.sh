de3260620b38e32647a0bae31cecaf1bf7ebc6c7
Christopher Plumberg <plumberg.1@osu.edu>
2020-03-18 21:31:23 +0100

    Update compile_and_run_checks.sh

--- src/HBT_event_generator/HBT_event_generator_w_errors/compile_and_run_checks.sh
+++ src/HBT_event_generator/HBT_event_generator_w_errors/compile_and_run_checks.sh
@@ -7,17 +7,51 @@
 	cd -
 	git pull && gmake distclean && gmake all
 
-	./run_checks.sh 5 0.02
-
 	./run_checks.sh 5 0.2
+
+	./run_checks.sh 5 0.5
+
+	./run_checks.sh 5 0.75
+
+	./run_checks.sh 5 1.0
+
+	./run_checks.sh 5 1.5
 
 	./run_checks.sh 5 2.0
 
-	./run_checks.sh 10 0.01
+	./run_checks.sh 5 5.0
 
-	./run_checks.sh 10 0.1
+	./run_checks.sh 5 7.5
+
+	./run_checks.sh 5 10.0
+
+	./run_checks.sh 5 15.0
+
+	./run_checks.sh 5 20.0
+
+	wait
+
+	./run_checks.sh 10 0.2
+
+	./run_checks.sh 10 0.5
+
+	./run_checks.sh 10 0.75
 
 	./run_checks.sh 10 1.0
+
+	./run_checks.sh 10 1.5
+
+	./run_checks.sh 10 2.0
+
+	./run_checks.sh 10 5.0
+
+	./run_checks.sh 10 7.5
+
+	./run_checks.sh 10 10.0
+
+	./run_checks.sh 10 15.0
+
+	./run_checks.sh 10 20.0
 
 
 ) &> compile_and_run_checks.out &
