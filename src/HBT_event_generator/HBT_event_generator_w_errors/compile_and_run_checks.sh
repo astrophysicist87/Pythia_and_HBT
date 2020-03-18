0fe0788bc33904482d30b3774b7adf46cc1134b7
Christopher Plumberg <plumberg.1@osu.edu>
2020-03-18 17:51:25 +0100

    Update compile_and_run_checks.sh

--- src/HBT_event_generator/HBT_event_generator_w_errors/compile_and_run_checks.sh
+++ src/HBT_event_generator/HBT_event_generator_w_errors/compile_and_run_checks.sh
@@ -7,12 +7,13 @@
 	rm -rf $RESULTSDIRECTORY
 	mkdir $RESULTSDIRECTORY
 	echo $RESULTSDIRECTORY > ./resultsDirectory.dat
-	cp ../parameters.dat $RESULTSDIRECTORY
 
 	cd ../../../../test_shifter
 	git pull && gmake distclean && gmake all
 
 	cd -
 	git pull && gmake distclean && gmake all && \
-	cp ../parameters.dat . && time ./run_checks.e &> run_checks.out
+		cp ../parameters.dat . && \
+		cp ../parameters.dat $RESULTSDIRECTORY && \
+		time ./run_checks.e &> run_checks.out
 ) &> compile_and_run_checks.out &
