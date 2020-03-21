ca6df75b27f2839c9f591da9c15780351c2bf735
Christopher Plumberg <plumberg.1@osu.edu>
2020-03-21 09:35:58 +0100

    Update run_checks.sh
    Fix typo

--- src/HBT_event_generator/HBT_event_generator_w_errors/run_checks.sh
+++ src/HBT_event_generator/HBT_event_generator_w_errors/run_checks.sh
@@ -9,7 +9,7 @@ RESULTSDIRECTORY=without_pair_density_Nev10000/results_${stem}
 rm -rf $RESULTSDIRECTORY
 mkdir -p $RESULTSDIRECTORY
 echo $RESULTSDIRECTORY > ./resultsDirectory.dat
-cp ../parameters.dat
+cp ../parameters.dat .
 cp ../parameters.dat $RESULTSDIRECTORY
 time ./run_checks.e RNG_R=$Rval RNG_a=$aval &> $RESULTSDIRECTORY/run_checks_${stem}.out &
 
