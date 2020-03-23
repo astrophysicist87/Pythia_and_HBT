f9e0a0120e571442d9c135a5c3324456fffd76da
Christopher Plumberg <plumberg.1@osu.edu>
2020-03-23 16:16:01 +0100

    Regenerate shifted CFs

--- src/HBT_event_generator/HBT_event_generator_w_errors/run_checks.sh
+++ src/HBT_event_generator/HBT_event_generator_w_errors/run_checks.sh
@@ -4,8 +4,8 @@ Rval=$1
 aval=$2
 stem=R`echo $Rval`_a`echo $aval`
 
-#RESULTSDIRECTORY=without_pair_density_Nev10000/results_${stem}
-RESULTSDIRECTORY=results_${stem}
+RESULTSDIRECTORY=without_pair_density_Nev10000/results_${stem}
+#RESULTSDIRECTORY=results_${stem}
 
 rm -rf $RESULTSDIRECTORY
 mkdir -p $RESULTSDIRECTORY
