799f3c71bae4b6570112c1e01b7cbf88ae2cebe4
Christopher Plumberg <plumberg.1@osu.edu>
2020-03-24 20:28:10 +0100

    Update generate_jobs.sh

--- generate_jobs.sh
+++ generate_jobs.sh
@@ -13,30 +13,9 @@ echo 'chosen_OMP_NUM_THREADS='$chosen_OMP_NUM_THREADS > omp_env.sh
 # set up array of job specifications
 
 declare -a specs=(
-	'useArbitraryParticle=true
-		projectile="p"
-		target="p"
-		beamEnergy="7000.0"
-		chosenHBTparticle="211"
-		Nevents=10000000
-		storeBjorkenCoordinates="false"
-		BEeffects="off"'
-	'useArbitraryParticle=true
-		projectile="p"
-		target="pPb"
-		beamEnergy="5020.0"
-		chosenHBTparticle="211"
-		Nevents=1000000
-		storeBjorkenCoordinates="false"
-		BEeffects="off"'
-	'useArbitraryParticle=true
-		projectile="Pb"
-		target="Pb"
-		beamEnergy="2760.0"
-		chosenHBTparticle="211"
-		Nevents=100000
-		storeBjorkenCoordinates="false"
-		BEeffects="off"'
+	'useArbitraryParticle=true projectile="p" target="p" beamEnergy="7000.0" chosenHBTparticle="211" Nevents=10000000 storeBjorkenCoordinates="false" BEeffects="off"'
+	'useArbitraryParticle=true projectile="p" target="Pb" beamEnergy="5020.0" chosenHBTparticle="211" Nevents=1000000 storeBjorkenCoordinates="false" BEeffects="off"'
+	'useArbitraryParticle=true projectile="Pb" target="Pb" beamEnergy="2760.0" chosenHBTparticle="211" Nevents=100000 storeBjorkenCoordinates="false" BEeffects="off"'
 )
 
 ########################################
