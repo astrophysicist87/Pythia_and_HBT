3bb99194dcd326bb5fbd16da2d624542e9ab2d32
Christopher Plumberg <plumberg.1@osu.edu>
2020-04-03 16:04:17 +0200

    Updates to develop branch

--- clean_all.sh
+++ clean_all.sh
@@ -11,6 +11,8 @@
 		rm -rf $1/results
 	}
 
+    (cd src/pythia8243; gmake distclean; cd ../..)
+
 	clean_directory src/pythia8243/examples
 
 	clean_directory src/HBT_event_generator/HBT_event_generator_w_errors
