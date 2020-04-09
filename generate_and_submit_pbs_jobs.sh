82d567da6d581504cb12b7a2d57ee6618836afb6
Christopher Plumberg <plumberg.1@osu.edu>
2020-04-09 16:33:12 +0200

    Update generate_and_submit_pbs_jobs.sh

--- generate_and_submit_pbs_jobs.sh
+++ generate_and_submit_pbs_jobs.sh
@@ -2,16 +2,17 @@
 
 (
 	# info for this run
-	NTHREADS=40
-	DIRECTORY=RESULTS2
-	PYTHIA_WALLTIME='12:00:00'
-	HBT_WALLTIME='48:00:00'
+	NTHREADS=1
+	DIRECTORY=RESULTS
+	PYTHIA_WALLTIME='01:00:00'
+	HBT_WALLTIME='01:00:00'
 
 	# Set job specifications here
 	declare -a specs=(
-		'useArbitraryParticle=true projectile="p" target="p" beamEnergy="7000.0" chosenHBTparticle="211" Nevents=10000000 storeBjorkenCoordinates="false" BEeffects="off"'
-		'useArbitraryParticle=true projectile="p" target="Pb" beamEnergy="5020.0" chosenHBTparticle="211" Nevents=1000000 storeBjorkenCoordinates="false" BEeffects="off"'
-		'useArbitraryParticle=true projectile="Pb" target="Pb" beamEnergy="2760.0" chosenHBTparticle="211" Nevents=100000 storeBjorkenCoordinates="false" BEeffects="off"'
+		'useArbitraryParticle=true projectile="p" target="p" beamEnergy="7000.0" chosenHBTparticle="211" Nevents=10 storeBjorkenCoordinates="false" BEeffects="off"'
+		#'useArbitraryParticle=true projectile="p" target="p" beamEnergy="7000.0" chosenHBTparticle="211" Nevents=10000000 storeBjorkenCoordinates="false" BEeffects="off"'
+		#'useArbitraryParticle=true projectile="p" target="Pb" beamEnergy="5020.0" chosenHBTparticle="211" Nevents=1000000 storeBjorkenCoordinates="false" BEeffects="off"'
+		#'useArbitraryParticle=true projectile="Pb" target="Pb" beamEnergy="2760.0" chosenHBTparticle="211" Nevents=100000 storeBjorkenCoordinates="false" BEeffects="off"'
 	)
 
 	#-----------------------------------------------------
