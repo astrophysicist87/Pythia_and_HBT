2f1d3195df28271e9bb87163f92d880c080d81c6
Christopher Plumberg <plumberg.1@osu.edu>
2020-05-05 16:42:17 +0200

    Update BoseEinstein.cc
    Found problematic chunk

--- src/pythia8243/src/BoseEinstein.cc
+++ src/pythia8243/src/BoseEinstein.cc
@@ -1018,6 +1018,7 @@ printf("Made it to %s::%d\n",__FUNCTION__, __LINE__);
 					const double EjQ = integrated_effective_source.at(jQ);
 					EnextQ = EjQ + ( nextQ - Qgrid.at(jQ) )
 										* ( integrated_effective_source.at(jQ+1) - EjQ ) / dQ;
+cout << jQ << "   " << Qgrid.size() << "   " << integrated_effective_source.size() << endl;
 
 				}
 				
