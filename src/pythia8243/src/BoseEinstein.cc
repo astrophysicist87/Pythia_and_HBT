a3c5aacbc79ada14528606c9695c52d4749868f5
Christopher Plumberg <plumberg.1@osu.edu>
2020-05-06 14:55:50 +0200

    Update BoseEinstein.cc

--- src/pythia8243/src/BoseEinstein.cc
+++ src/pythia8243/src/BoseEinstein.cc
@@ -976,11 +976,11 @@ void BoseEinstein::set_RHS(
 
 			RHS.push_back( std::make_pair( thisQ, result ) );
 
-			if ( pairCount == 0 )
+			/*if ( pairCount == 0 )
 			{
 				pairCount++;
 				continue;
-			}
+			}*/
 
 			if ( pairCount == (int)sorted_xDiffs.size() )
 				continue;
