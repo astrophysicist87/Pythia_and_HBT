e1b967a6ca88885dacd8ce556bde615e17896edb
Christopher Plumberg <plumberg.1@osu.edu>
2020-05-14 17:48:21 +0200

    Try comparing Pythia shifting modes WITH HBT analyses

--- generate_and_submit_pbs_jobs.sh
+++ generate_and_submit_pbs_jobs.sh
@@ -2,19 +2,18 @@
 
 (
 	# info for this run
-	NTHREADS=12
-	NDATASETS=1
-	DIRECTORY=test_RESULTS
-	PYTHIA_WALLTIME='01:00:00'
-	HBT_WALLTIME='01:00:00'
+	NTHREADS=128
+	NDATASETS=10
+	DIRECTORY=RESULTS_test_pp_shifting_modes
+	PYTHIA_WALLTIME='96:00:00'
+	HBT_WALLTIME='72:00:00'
 
 	# Set job specifications here
 	declare -a specs=(
-		'projectile="p" target="p" beamEnergy="13000" Nevents=1000'
-		#'projectile="p" target="p" beamEnergy="13000" chosenHBTparticle="211" Nevents=100000 BEeffects="on" shiftingSet="1" compensationSet="0" compensationMode="1" runHBTEG="false" runFitCF="false" runSV="false"'
-		#'projectile="p" target="p" beamEnergy="13000" chosenHBTparticle="211" Nevents=100000 BEeffects="on" shiftingSet="1" compensationSet="2" compensationMode="1" runHBTEG="false" runFitCF="false" runSV="false"'
-		#'projectile="p" target="p" beamEnergy="13000" chosenHBTparticle="211" Nevents=100000 BEeffects="on" shiftingSet="0" compensationSet="1" compensationMode="0" runHBTEG="false" runFitCF="false" runSV="false"'
-		#'projectile="p" target="p" beamEnergy="13000" chosenHBTparticle="211" Nevents=100000 BEeffects="on" shiftingSet="1" compensationSet="1" compensationMode="0" runHBTEG="false" runFitCF="false" runSV="false"'
+		'projectile="p" target="p" beamEnergy="13000" chosenHBTparticle="211" Nevents=1000000 BEeffects="on" shiftingSet="1" compensationSet="0" compensationMode="1" eventClassSelectionMode="multiplicity" runSV="false"'
+		'projectile="p" target="p" beamEnergy="13000" chosenHBTparticle="211" Nevents=1000000 BEeffects="on" shiftingSet="1" compensationSet="2" compensationMode="1" eventClassSelectionMode="multiplicity" runSV="false"'
+		'projectile="p" target="p" beamEnergy="13000" chosenHBTparticle="211" Nevents=1000000 BEeffects="on" shiftingSet="0" compensationSet="1" compensationMode="0" eventClassSelectionMode="multiplicity" runSV="false"'
+		'projectile="p" target="p" beamEnergy="13000" chosenHBTparticle="211" Nevents=1000000 BEeffects="on" shiftingSet="1" compensationSet="1" compensationMode="0" eventClassSelectionMode="multiplicity" runSV="false"'
 	)
 
 	#-----------------------------------------------------
