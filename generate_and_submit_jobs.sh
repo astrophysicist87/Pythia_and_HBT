28edf0b121dd21e3607b018cbd5b2408d6de2e38
Christopher Plumberg <plumberg.1@osu.edu>
2020-05-19 19:05:47 +0200

    Undo...

--- generate_and_submit_jobs.sh
+++ generate_and_submit_jobs.sh
@@ -7,7 +7,7 @@
 
 	# Set job specifications here
 	#declare -a class_ranges=("1-11" "12-16" "17-22" "23-28" "29-34" "35-41" "42-51" "52-151" "152-1000000")
-	declare -a class_ranges=("0-5%" "5-10%" "10-20%" "20-30%" "30-40%" "40-60%" "60-100%")
+	#declare -a class_ranges=("0-5%" "5-10%" "10-20%" "20-30%" "30-40%" "40-60%" "60-100%")
 	declare -a specs=(
 		'projectile="p" target="p" beamEnergy="13000" chosenHBTparticle="211" eventClassSelectionMode="centrality" Nevents=1000 BEeffects="off" runSV="false"'
 	)
@@ -26,7 +26,7 @@
 	source scripts/export_specs.sh
 	SPECS_FULL_PATH=`readlink -f scripts/specs.sh`
 	export_specs_array "${specs[@]}" > $SPECS_FULL_PATH
-	export_specs_array "${class_ranges[@]}" >> $SPECS_FULL_PATH
+	#export_specs_array "${class_ranges[@]}" >> $SPECS_FULL_PATH
 
 	# Generate jobs for these specifications
 	DIRECTORY_FULL_PATH=`readlink -f $DIRECTORY`
