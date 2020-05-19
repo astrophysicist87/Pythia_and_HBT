29e72685a192d055fe1c3653d9c574fa85858cf1
Christopher Plumberg <plumberg.1@osu.edu>
2020-05-19 20:01:51 +0200

    Passing class ranges correctly

--- scripts/export_specs.sh
+++ scripts/export_specs.sh
@@ -1,5 +1,18 @@
 #! /usr/bin/env bash
 
+function export_class_range_array () {
+
+loc_class_ranges=("$@")
+
+echo 'declare -a class_ranges=('
+for CR in "${loc_class_ranges[@]}"
+do
+    echo \'$CR\'
+done
+echo ')'
+
+}
+
 function export_specs_array () {
 
 loc_specs=("$@")
@@ -13,4 +26,5 @@ echo ')'
 
 }
 
+export -f export_class_range_array
 export -f export_specs_array
