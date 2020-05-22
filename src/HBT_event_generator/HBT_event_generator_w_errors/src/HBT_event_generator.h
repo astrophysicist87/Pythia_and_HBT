3dbded43d6942ac0a151023e323dc9c61abf98ec
Christopher Plumberg <plumberg.1@osu.edu>
2020-05-22 13:43:31 +0200

    Stupidy typos...

--- src/HBT_event_generator/HBT_event_generator_w_errors/src/HBT_event_generator.h
+++ src/HBT_event_generator/HBT_event_generator_w_errors/src/HBT_event_generator.h
@@ -231,7 +231,7 @@ class HBT_event_generator
 		void Output_correlation_function_q_mode_1D( string filename );
 
 		void Dump_state(string path);
-		void Load_state(string path);
+		//void Load_state(string path);
 
 };
 
--- src/HBT_event_generator/HBT_event_generator_w_errors/src/StateInfo.cpp
+++ src/HBT_event_generator/HBT_event_generator_w_errors/src/StateInfo.cpp
@@ -46,60 +46,60 @@ void HBT_event_generator::Dump_state(string path)
 	return;
 }
 
-
-void HBT_event_generator::Load_state(string path)
-{
-	const int q_space_size = ( q_mode == 0 ) ?
-								n_qo_bins*n_qs_bins*n_ql_bins :
-								n_Q_bins;
-	const int K_space_size = n_KT_bins*n_Kphi_bins*n_KL_bins;
-	double dummy = 0.0;
-
-	ifstream inPairs( path + "/state/pairs.dat" );
-	for ( int iK = 0; iK < K_space_size; iK++ )
-	{
-		/*inPairs
-			>> numPair[iK]
-			>> numPair2[iK]
-			>> denPair[iK]
-			>> denPair2[iK];*/
-		inPairs >> dummy; numPair[iK]  += static_cast<int>( dummy );
-		inPairs >> dummy; numPair2[iK] += static_cast<int>( dummy );
-		inPairs >> dummy; denPair[iK]  += static_cast<int>( dummy );
-		inPairs >> dummy; denPair2[iK] += static_cast<int>( dummy );
-	}
-	inPairs.close();
-
-	ifstream inDistributions( path + "/state/distributions.dat" );
-	for ( int iqK = 0; iqK < K_space_size*q_space_size; iqK++ )
-	{
-		/*inDistributions
-			>> numerator[iqK]
-			>> denominator[iqK]
-			>> numerator2[iqK]
-			>> denominator2[iqK]
-			>> numerator_denominator[iqK]
-			//>> denominator_cell_was_filled[iqK]
-			>> dummy
-			>> numerator_bin_count[iqK]
-			>> denominator_bin_count[iqK]
-			>> numerator_numPair[iqK]
-			>> denominator_denPair[iqK];
-		denominator_cell_was_filled[iqK] = static_cast<bool>( dummy );*/
-		inDistributions >> dummy; numerator[iqK]                   += dummy;
-		inDistributions >> dummy; denominator[iqK]                 += dummy;
-		inDistributions >> dummy; numerator2[iqK]                  += dummy;
-		inDistributions >> dummy; denominator2[iqK]                += dummy;
-		inDistributions >> dummy; numerator_denominator[iqK]       += dummy;
-		inDistributions >> dummy; denominator_cell_was_filled[iqK] += static_cast<bool>( dummy );
-		inDistributions >> dummy; numerator_bin_count[iqK]         += static_cast<int>( dummy );
-		inDistributions >> dummy; denominator_bin_count[iqK]       += static_cast<int>( dummy );
-		inDistributions >> dummy; numerator_numPair[iqK]           += dummy;
-		inDistributions >> dummy; denominator_denPair[iqK]         += dummy;
-	}
-	inDistributions.close();
-
-	return;
-}
+//
+//void HBT_event_generator::Load_state(string path)
+//{
+//	const int q_space_size = ( q_mode == 0 ) ?
+//								n_qo_bins*n_qs_bins*n_ql_bins :
+//								n_Q_bins;
+//	const int K_space_size = n_KT_bins*n_Kphi_bins*n_KL_bins;
+//	double dummy = 0.0;
+//
+//	ifstream inPairs( path + "/state/pairs.dat" );
+//	for ( int iK = 0; iK < K_space_size; iK++ )
+//	{
+//		/*inPairs
+//			>> numPair[iK]
+//			>> numPair2[iK]
+//			>> denPair[iK]
+//			>> denPair2[iK];*/
+//		inPairs >> dummy; numPair[iK]  += static_cast<int>( dummy );
+//		inPairs >> dummy; numPair2[iK] += static_cast<int>( dummy );
+//		inPairs >> dummy; denPair[iK]  += static_cast<int>( dummy );
+//		inPairs >> dummy; denPair2[iK] += static_cast<int>( dummy );
+//	}
+//	inPairs.close();
+//
+//	ifstream inDistributions( path + "/state/distributions.dat" );
+//	for ( int iqK = 0; iqK < K_space_size*q_space_size; iqK++ )
+//	{
+//		/*inDistributions
+//			>> numerator[iqK]
+//			>> denominator[iqK]
+//			>> numerator2[iqK]
+//			>> denominator2[iqK]
+//			>> numerator_denominator[iqK]
+//			//>> denominator_cell_was_filled[iqK]
+//			>> dummy
+//			>> numerator_bin_count[iqK]
+//			>> denominator_bin_count[iqK]
+//			>> numerator_numPair[iqK]
+//			>> denominator_denPair[iqK];
+//		denominator_cell_was_filled[iqK] = static_cast<bool>( dummy );*/
+//		inDistributions >> dummy; numerator[iqK]                   += dummy;
+//		inDistributions >> dummy; denominator[iqK]                 += dummy;
+//		inDistributions >> dummy; numerator2[iqK]                  += dummy;
+//		inDistributions >> dummy; denominator2[iqK]                += dummy;
+//		inDistributions >> dummy; numerator_denominator[iqK]       += dummy;
+//		inDistributions >> dummy; denominator_cell_was_filled[iqK] += static_cast<bool>( dummy );
+//		inDistributions >> dummy; numerator_bin_count[iqK]         += static_cast<int>( dummy );
+//		inDistributions >> dummy; denominator_bin_count[iqK]       += static_cast<int>( dummy );
+//		inDistributions >> dummy; numerator_numPair[iqK]           += dummy;
+//		inDistributions >> dummy; denominator_denPair[iqK]         += dummy;
+//	}
+//	inDistributions.close();
+//
+//	return;
+//}
 
 // End of file
