8558bd596621f65856506aba96f1fa92ba119f3c
Christopher Plumberg <plumberg.1@osu.edu>
2020-05-25 12:35:52 +0200

    Now try smaller n_mix value

--- generate_and_submit_jobs.sh
+++ generate_and_submit_jobs.sh
@@ -3,7 +3,7 @@
 (
 	# info for this run
 	NTHREADS=12
-	DIRECTORY=results_blixen_nmix1000
+	DIRECTORY=results_blixen_nmix10
 
 	# Set job specifications here
 	#declare -a class_ranges=("1-11" "12-16" "17-22" "23-28" "29-34" "35-41" "42-51" "52-151" "152-1000000")
