2a0b90cdba4aae42fba6bce3a4d2e28a169fbdc4
Christopher Plumberg <plumberg.1@osu.edu>
2020-05-26 11:22:15 +0200

    Update generate_and_submit_jobs.sh
    Submission to lema

--- generate_and_submit_jobs.sh
+++ generate_and_submit_jobs.sh
@@ -3,7 +3,7 @@
 (
 	# info for this run
 	NTHREADS=64
-	DIRECTORY=results_egan
+	DIRECTORY=results_lema
 
 	# Set job specifications here
 	declare -a class_ranges=("1-11" "12-16" "17-22" "23-28" "29-34" "35-41" "42-51" "52-151" "152-1000000")
@@ -11,10 +11,10 @@
 	#declare -a class_ranges=("0-100%")
 	declare -a specs=(
 		#'projectile="p" target="p" beamEnergy="13000" chosenHBTparticle="211" Nevents=100000 BEeffects="on" BEEnhancementMode="1" eventClassSelectionMode="centrality" runSV="false"'
-		'projectile="p" target="p" beamEnergy="7000" chosenHBTparticle="211" Nevents=48000000 BEeffects="on" shiftingSet="1" compensationSet="0" compensationMode="1" eventClassSelectionMode="multiplicity" runSV="false"'
-		'projectile="p" target="p" beamEnergy="7000" chosenHBTparticle="211" Nevents=48000000 BEeffects="on" shiftingSet="1" compensationSet="2" compensationMode="1" eventClassSelectionMode="multiplicity" runSV="false"'
-		#'projectile="p" target="p" beamEnergy="7000" chosenHBTparticle="211" Nevents=10000000 BEeffects="on" shiftingSet="0" compensationSet="1" compensationMode="0" eventClassSelectionMode="multiplicity" runSV="false"'
-		#'projectile="p" target="p" beamEnergy="7000" chosenHBTparticle="211" Nevents=10000000 BEeffects="on" shiftingSet="1" compensationSet="1" compensationMode="0" eventClassSelectionMode="multiplicity" runSV="false"'
+		#'projectile="p" target="p" beamEnergy="7000" chosenHBTparticle="211" Nevents=48000000 BEeffects="on" shiftingSet="1" compensationSet="0" compensationMode="1" eventClassSelectionMode="multiplicity" runSV="false"'
+		#'projectile="p" target="p" beamEnergy="7000" chosenHBTparticle="211" Nevents=48000000 BEeffects="on" shiftingSet="1" compensationSet="2" compensationMode="1" eventClassSelectionMode="multiplicity" runSV="false"'
+		'projectile="p" target="p" beamEnergy="7000" chosenHBTparticle="211" Nevents=10000000 BEeffects="on" shiftingSet="0" compensationSet="1" compensationMode="0" eventClassSelectionMode="multiplicity" runSV="false"'
+		'projectile="p" target="p" beamEnergy="7000" chosenHBTparticle="211" Nevents=10000000 BEeffects="on" shiftingSet="1" compensationSet="1" compensationMode="0" eventClassSelectionMode="multiplicity" runSV="false"'
 		#'projectile="p" target="p" beamEnergy="7000" chosenHBTparticle="211" Nevents=10000000 BEeffects="on" BEEnhancementMode="1" eventClassSelectionMode="multiplicity" runSV="false"'
 	)
 
