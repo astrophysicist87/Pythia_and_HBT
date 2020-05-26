e3e70a80be15d8f8f308196d47ed055f106f26de
Christopher Plumberg <plumberg.1@osu.edu>
2020-05-26 05:07:05 +0200

    Restore more complete job options with speed-up now implemented.

--- generate_and_submit_jobs.sh
+++ generate_and_submit_jobs.sh
@@ -11,8 +11,8 @@
 	#declare -a class_ranges=("0-100%")
 	declare -a specs=(
 		#'projectile="p" target="p" beamEnergy="13000" chosenHBTparticle="211" Nevents=100000 BEeffects="on" BEEnhancementMode="1" eventClassSelectionMode="centrality" runSV="false"'
-		'projectile="p" target="p" beamEnergy="7000" chosenHBTparticle="211" Nevents=10000000 BEeffects="on" shiftingSet="1" compensationSet="0" compensationMode="1" eventClassSelectionMode="multiplicity" runSV="false"'
-		'projectile="p" target="p" beamEnergy="7000" chosenHBTparticle="211" Nevents=10000000 BEeffects="on" shiftingSet="1" compensationSet="2" compensationMode="1" eventClassSelectionMode="multiplicity" runSV="false"'
+		'projectile="p" target="p" beamEnergy="7000" chosenHBTparticle="211" Nevents=48000000 BEeffects="on" shiftingSet="1" compensationSet="0" compensationMode="1" eventClassSelectionMode="multiplicity" runSV="false"'
+		'projectile="p" target="p" beamEnergy="7000" chosenHBTparticle="211" Nevents=48000000 BEeffects="on" shiftingSet="1" compensationSet="2" compensationMode="1" eventClassSelectionMode="multiplicity" runSV="false"'
 		#'projectile="p" target="p" beamEnergy="7000" chosenHBTparticle="211" Nevents=10000000 BEeffects="on" shiftingSet="0" compensationSet="1" compensationMode="0" eventClassSelectionMode="multiplicity" runSV="false"'
 		#'projectile="p" target="p" beamEnergy="7000" chosenHBTparticle="211" Nevents=10000000 BEeffects="on" shiftingSet="1" compensationSet="1" compensationMode="0" eventClassSelectionMode="multiplicity" runSV="false"'
 		#'projectile="p" target="p" beamEnergy="7000" chosenHBTparticle="211" Nevents=10000000 BEeffects="on" BEEnhancementMode="1" eventClassSelectionMode="multiplicity" runSV="false"'
