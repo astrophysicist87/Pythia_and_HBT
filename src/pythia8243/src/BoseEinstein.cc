2947c3f93e98b781f2bf33670aabd08079e2438d
Christopher Plumberg <plumberg.1@osu.edu>
2020-06-02 11:35:58 +0200

    Update BoseEinstein.cc
    Revert to original to see difference.

--- src/pythia8243/src/BoseEinstein.cc
+++ src/pythia8243/src/BoseEinstein.cc
@@ -454,11 +454,11 @@ bool BoseEinstein::shiftEvent( Event& event )
 		pHad.p.e( sqrt( pHad.p.pAbs2() + pHad.m2 ) );
 		eSumShifted  += pHad.p.e();
 		//epSumShifted += abs(pHad.p.e()) + abs(pHad.p.px()) + abs(pHad.p.py()) + abs(pHad.p.pz());
-		//eDiffByComp  += dot3( pHad.pComp, pHad.p ) / pHad.p.e();	//ORIGINAL
-		eDiffByComp  += sqrt( pHad.p.e()*pHad.p.e()
+		eDiffByComp  += dot3( pHad.pComp, pHad.p ) / pHad.p.e();	//ORIGINAL
+		/*eDiffByComp  += sqrt( pHad.p.e()*pHad.p.e()
                               + 2.0*dot3( pHad.pComp, pHad.p )
                               + dot3( pHad.pComp, pHad.pComp ) )
-                        - pHad.p.e();
+                        - pHad.p.e();*/
 /*cout << "COMPARE eDiffByComp defs: " << dot3( pHad.pComp, pHad.p ) / pHad.p.e()
 		<< " vs. " << sqrt( pHad.p.e()*pHad.p.e()
                       + 2.0*dot3( pHad.pComp, pHad.p )
@@ -502,11 +502,11 @@ cout << setprecision(12) << "Compensation check(2): " << iStep << "   "
 			pHad.p      += compFac * pHad.pComp;
 			pHad.p.e( sqrt( pHad.p.pAbs2() + pHad.m2 ) );
 			eSumShifted += pHad.p.e();
-			//eDiffByComp  += dot3( pHad.pComp, pHad.p ) / pHad.p.e();	//ORIGINAL
-			eDiffByComp  += sqrt( pHad.p.e()*pHad.p.e()
+			eDiffByComp  += dot3( pHad.pComp, pHad.p ) / pHad.p.e();	//ORIGINAL
+			/*eDiffByComp  += sqrt( pHad.p.e()*pHad.p.e()
 	                              + 2.0*dot3( pHad.pComp, pHad.p )
 	                              + dot3( pHad.pComp, pHad.pComp ) )
-	                        - pHad.p.e();
+	                        - pHad.p.e();*/
 /*cout << "COMPARE eDiffByComp defs: " << dot3( pHad.pComp, pHad.p ) / pHad.p.e()
 		<< " vs. " << sqrt( pHad.p.e()*pHad.p.e()
                       + 2.0*dot3( pHad.pComp, pHad.p )
