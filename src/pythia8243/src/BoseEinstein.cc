ee9b8a3e22a56f8ed609877416d6ac515e369147
Christopher Plumberg <plumberg.1@osu.edu>
2020-06-05 18:08:35 +0200

    Run same jobs on lema, with new compensation version = 2.

--- src/pythia8243/src/BoseEinstein.cc
+++ src/pythia8243/src/BoseEinstein.cc
@@ -443,7 +443,7 @@ bool BoseEinstein::shiftEvent( Event& event )
 	double eSumOriginal = 0.;
 	double eSumShifted  = 0.;
 	double eDiffByComp  = 0.;
-	constexpr int compensation_version_to_use = 0;
+	constexpr int compensation_version_to_use = 2;
 
 	constexpr bool perform_compensation = true;
 	constexpr bool check_for_bad_events = true;
