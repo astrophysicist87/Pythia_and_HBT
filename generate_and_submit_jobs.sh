1add950ca8207b9f431763c858ca4bd82a207d15
Christopher Plumberg <plumberg.1@osu.edu>
2020-06-10 17:27:11 +0200

    Update generate_and_submit_jobs.sh
    Fix another typo.

--- generate_and_submit_jobs.sh
+++ generate_and_submit_jobs.sh
@@ -2,9 +2,9 @@
 
 (
 	# info for this run
-	NTHREADS=16
-	DIRECTORY=results_vishnu_N10000000
-	NICENESS=0
+	NTHREADS=64
+	DIRECTORY=results_lema_N10000000
+	NICENESS=19
 
 	# Set job specifications here
 	#declare -a class_ranges=("1-11" "12-16" "17-22" "23-28" "29-34" "35-41" "42-51" "52-151" "152-1000000")
