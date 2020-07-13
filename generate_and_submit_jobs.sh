cc9c7b0b91154c559b233807209f7d5e8a25a3e2
Christopher Plumberg <plumberg.1@osu.edu>
2020-07-13 11:38:09 +0200

    Update generate_and_submit_jobs.sh
    Interfacing with new v8303 appears to have been done correctly.  Submitting full job to egan.

--- generate_and_submit_jobs.sh
+++ generate_and_submit_jobs.sh
@@ -2,17 +2,17 @@
 
 (
 	# info for this run
-	NTHREADS=1
-	DIRECTORY=results_pp_7TeV_N60000000_wRescattering
-	NICENESS=0
+	NTHREADS=64
+	DIRECTORY=results_pp_7TeV_Nev60000000_wRescattering
+	NICENESS=19
 	#NTHREADS=12
 	#DIRECTORY=results_blixen_PbPb2760GeV_Nev1000
 	#NICENESS=0
 
 	# Set job specifications here
-	#declare -a class_ranges=("1-11" "12-16" "17-22" "23-28" "29-34" "35-41" "42-51" "52-151")
+	declare -a class_ranges=("1-11" "12-16" "17-22" "23-28" "29-34" "35-41" "42-51" "52-151")
 	#declare -a class_ranges=("0-5%" "5-10%" "10-20%" "20-30%" "30-40%" "40-60%" "60-100%")
-	declare -a class_ranges=("0-100%")
+	#declare -a class_ranges=("0-100%")
 	declare -a specs=(
 		##############
 		#blixen
@@ -21,8 +21,7 @@
 		##############
 		#egan
 		##############
-		#'projectile="p" target="p" beamEnergy="7000" Nevents=60000000 eventClassSelectionMode="multiplicity" SetPartonVertices="on"'
-		'projectile="p" target="p" beamEnergy="7000" Nevents=10 eventClassSelectionMode="multiplicity" SetPartonVertices="on"'
+		'projectile="p" target="p" beamEnergy="7000" Nevents=60000000 eventClassSelectionMode="multiplicity" SetPartonVertices="on"'
 		##############
 		#lema
 		##############
