b67dea84554b8bc266e7d85368fbd072faf06251
Christopher Plumberg <plumberg.1@osu.edu>
2020-08-04 20:56:26 +0200

    Update generate_and_submit_jobs.sh

--- generate_and_submit_jobs.sh
+++ generate_and_submit_jobs.sh
@@ -4,7 +4,8 @@
 	# info for this run
 	NTHREADS=32
 	#DIRECTORY=results_PbPb_2.76TeV_Nev10000
-	DIRECTORY=results_egan_test2
+	#DIRECTORY=results_egan_test2
+	DIRECTORY=results_pp_7TeV_Nev30000000_V0A
 	NICENESS=19
 	#NTHREADS=12
 	#DIRECTORY=results_blixen_PbPb2760GeV_Nev1000
@@ -12,7 +13,7 @@
 
 	# Set job specifications here
 	#declare -a class_ranges=("1-11" "12-16" "17-22" "23-28" "29-34" "35-41" "42-51" "52-151")
-	declare -a class_ranges=("0-1%" "1-5%" "5-10%" "10-20%" "20-30%" "30-40%" "40-60%" "60-100%")
+	declare -a class_ranges=("0-20%" "20-40%" "40-60%" "60-100%")
 	#declare -a class_ranges=("0-100%")
 	declare -a specs=(
 		##############
@@ -22,8 +23,8 @@
 		##############
 		#egan
 		##############
-		#'projectile="p" target="p" beamEnergy="7000" Nevents=60000000 eventClassSelectionMode="multiplicity" SetPartonVertices="on"'
-		'projectile="p" target="p" beamEnergy="7000" Nevents=1000000'
+		'projectile="p" target="p" beamEnergy="7000" Nevents=60000000 SetPartonVertices="on"'
+		#'projectile="p" target="p" beamEnergy="7000" Nevents=1000000'
 		##############
 		#lema
 		##############
