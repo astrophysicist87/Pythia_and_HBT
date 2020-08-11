f0aa38c6fa0216a7f3fffb65d0c7b0adec833f29
Christopher Plumberg <plumberg.1@osu.edu>
2020-08-11 16:05:43 +0200

    Stick LSQ back in.

--- src/HBT_event_generator/fit_correlation_function/src/correlation_function.cpp
+++ src/HBT_event_generator/fit_correlation_function/src/correlation_function.cpp
@@ -146,11 +146,11 @@ void Correlation_function::initialize_all(
 	// Read in correlation function
 	Load_correlation_function( filepath_in );
 
-	bool use_Gaussian_fits = false;
+	bool use_Gaussian_fits = true;
 	if ( use_Gaussian_fits )
 	{
 		out << "<<< USING NEW GAUSSIAN FITS >>>" << endl;
-		//Fit_correlation_function_GF_LSQ();
+		Fit_correlation_function_GF_LSQ();
 	}
 	else
 	{
