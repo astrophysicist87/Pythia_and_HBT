e5bed0a44908b50820ffc370db72e2a1303197cf
Christopher Plumberg <plumberg.1@osu.edu>
2020-08-11 17:31:53 +0200

    Testing how to compile with macros

---
+++ src/HBT_event_generator/macro_test/fit_v1.h
@@ -0,0 +1,28 @@
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_multifit_nlin.h>
+
+#define N 40
+
+struct data {
+   size_t n;
+   double * y;
+   double * sigma;
+ };
+ 
+ int
+ expb_f (const gsl_vector * x, void *data, 
+         gsl_vector * f);
+ int
+ expb_df (const gsl_vector * x, void *data, 
+          gsl_matrix * J);
+ int
+ expb_fdf (const gsl_vector * x, void *data,
+           gsl_vector * f, gsl_matrix * J);
+
+void print_state (size_t iter, gsl_multifit_fdfsolver * s);
+
+void fit_v1_driver();
+
---
+++ src/HBT_event_generator/macro_test/test.cpp
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "fit_v1.h"
+//#include "fit_v2.h"
+
+int main (void)
+{
+	fit_v1_driver();
+
+	return 0;
+}
